ff9fe0bba89de377bc5782c42e2616b27bb2a926
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-11-03 15:49:09 +0600

    Style books list

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,12 @@
 module BooksHelper
+  def format_authors(authors)
+    authors.map do |author|
+      #link_to h(author.name), author
+      h(author.name)
+    end.join(',')
+  end
+
+  def link_to_book(book)
+    link_to h(book.title), book
+  end
 end
