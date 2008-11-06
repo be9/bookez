f30e578ec35c27269d5ed15c47dadd3528a0bbe0
Parfinenko Vladimir <parfinenko.vladimir@gmail.com>
2008-11-06 23:20:35 +0600

    Changed sorting of books in author-page

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,11 +1,13 @@
 class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
-    @books = @author.books.sort_by { |book| get_position_in_authors(@author, book) }
+    @books = @author.books.sort_by { |book| book.title }
+
+    #@books = @author.books.sort_by { |book| get_position_in_authors(@author, book) }
   end
 
   private
-  def get_position_in_authors(author, book)
-    Authorship.find(:first, :conditions => ["book_id=? AND author_id=?", "#{book.id}", "#{author.id}"]).position
-  end
+  #def get_position_in_authors(author, book)
+  #  Authorship.find(:first, :conditions => ["book_id=? AND author_id=?", "#{book.id}", "#{author.id}"]).position
+  #end
 end
