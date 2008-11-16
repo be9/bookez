d1cd068c57464e0bd6283e0e1d6627f573911b7a
Parfinenko Vladimir <cypok@cypok-laptop.(none)>
2008-11-16 21:29:26 +0600

    Written SearchController#by_title

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,37 +6,51 @@ class SearchController < ApplicationController
     query = (params[:query] || '').split
     books_rating = {} # book => rating
     query.each do |word|
-      # find every word in title, orig_title and author
-      Book.all(:conditions => ["title LIKE ? OR orig_title LIKE ?", "%#{word}%", "%#{word}%"]).each do |book|
+      search_book(:title, word).each do |book|
         add_book_by_rating(books_rating, book, 2)
       end
       
-      # find in annotation but with less rating
-      Book.all(:conditions => ["annotation LIKE ?", "%#{word}%"]).each do |book|
+      search_book(:orig_title, word).each do |book|
+        add_book_by_rating(books_rating, book, 1.5)
+      end
+      
+      search_book(:annotation, word).each do |book|
         add_book_by_rating(books_rating, book, 1)
       end
       
       # find in authors
-      Author.all(:conditions => ["name LIKE ?", "%#{word}%"]).each do |author|
-        add_author_books(books_rating, author)
+      search_author(word).each do |author|
+        add_author_books(books_rating, author, 1, 2)
       end
     end
+
     # sort books by rating (descending)
     @books = (books_rating.sort_by { |x| -x[1] } ).map { |x| x[0] }
   end
  
   def by_title
     if true #xhr?
-       render :text => "123\n456\nabcdef\nВася"
+       render :text => search_book(:title, params[:q]).merge(search_book(:orig_title, params[:q])).map( &:title ).join("\n")
     else
        render :text => "XHR only", :status => 403
     end
   end
  
   private
-  def add_author_books(hash, author)
+  def search_book(field, str)  
+    # find str in book's field
+    Book.all(:conditions => ["#{field} LIKE ?", "%#{str}%"])
+  end
+
+  def search_author(str)  
+    # find author by name
+    Author.all(:conditions => ["name LIKE ?", "%#{str}%"])
+  end
+
+  def add_author_books(hash, author, min_rating, max_rating)
     author.get_books_with_position.each do |book, pos|
-      add_book_by_rating( hash, book, 1 + 0.5**pos )
+      add_book_by_rating( hash, book,
+        min_rating + (max_rating - min_rating) * 0.5**pos)
     end
   end
 
