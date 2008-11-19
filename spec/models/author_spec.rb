46ddf527c95d01635076e02350e7f40849107f81
Parfinenko Vladimir <cypok@cypok-laptop.(none)>
2008-11-19 18:50:19 +0600

    Add spec for Author model

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -10,4 +10,16 @@ describe Author do
   it "should create a new instance given valid attributes" do
     Author.create!(@valid_attributes)
   end
+
+  it "#get_books_with_positon should return [book - pos] array" do
+    author = Author.create(@valid_attributes)
+
+    as = mock Authorship
+    as.stub!(:book).and_return("11", "22", "33")
+    as.stub!(:position).and_return(5, 6, 7)
+
+    author.stub!(:authorships).and_return([ as, as, as ])
+
+    author.get_books_with_position.should == [ ["11", 5],  ["22", 6], ["33", 7] ]
+  end
 end
