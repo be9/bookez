ffdbef78de0102eaa9cdf5ac7043d4b39ba99f3d
Parfinenko Vladimir <cypok@cypok-laptop.(none)>
2008-11-21 21:51:17 +0600

    Autocomplete added to Authors and OrigTitle

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -30,12 +30,24 @@ class SearchController < ApplicationController
  
   def by_title
     if true #xhr?
-       render :text => (search_book(:title, params[:q]) | search_book(:orig_title, params[:q])).map( &:title ).join("\n")
+      render :text => (search_book(:title, params[:q]) | search_book(:orig_title, params[:q]))[0, params[:limit].to_i].map( &:title ).join("\n")
     else
-       render :text => "XHR only", :status => 403
+      render :text => "XHR only", :status => 403
     end
   end
- 
+  
+  def by_author
+    if true #xhr?
+      q = params[:q]
+      author = q.split(",").last
+      static = q[0, q.length - author.length] # cut off author
+
+      render :text => search_author( author.strip )[0, params[:limit].to_i].map { |x| "#{static}#{x.name}" }.join("\n")
+    else
+      render :text => "XHR only", :status => 403
+    end
+  end
+
   private
   def search_book(field, str)  
     # find str in book's field
