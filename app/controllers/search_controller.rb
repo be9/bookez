6a9a678c3714ddbc74ae7efb328d4c9d7dae9648
Parfinenko Vladimir <cypok@cypok-laptop.(none)>
2009-01-31 18:44:53 +0600

    Added XHR checking in search controller and setting of default limit.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,18 +10,20 @@ class SearchController < ApplicationController
   end
  
   def by_title
-    if true #xhr?
-      render :text => (search_book(:title, params[:q]) | search_book(:orig_title, params[:q]))[0, params[:limit].to_i].map( &:title ).join("\n")
+    if request.xhr?
+      params[:limit] ||= 30
+      render :text => (search_book(:title, params[:q]))[0, params[:limit].to_i].map( &:title ).join("\n")
     else
       render :text => "XHR only", :status => 403
     end
   end
   
   def by_author
-    if true #xhr?
+    if request.xhr?
       q = params[:q]
       author = q.split(",").last.lstrip
       static = q[0, q.length - author.length] # cut off author
+      params[:limit] ||= 30
 
       render :text => search_author( author.strip )[0, params[:limit].to_i].map { |x| "#{static}#{x.name}" }.join("\n")
     else
